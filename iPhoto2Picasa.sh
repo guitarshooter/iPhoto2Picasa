0055754c105f102fc70ece2376715695339203a4
shooter@Mac <shooter@artemis.local>
2012-12-02 22:39:32 +0900

    動画にも対応

--- iPhoto2Picasa.sh
+++ iPhoto2Picasa.sh
@@ -29,27 +29,36 @@ for dir in `find . -d 4 -print`
 do
   targetdir=`basename "$dir"`
   dirnum=`echo $targetdir|sed -e 's/-//'`
-  if [ $dirnum -gt $LASTDATE ];then
-    for file in $dir/*.JPG
+  if [ "$dirnum" -gt $LASTDATE ];then
+    for file in $dir/* #フルパス
     do
-      filename=`basename $file`
-      albumname=`stat -l -t %Y/%m/%d $file|cut -f6 -d" "`
+      filename=`basename $file` #ファイル名のみ
+      albumname=`stat -l -t %Y/%m/%d "$file"|cut -f6 -d" "`
+      ext=`echo ${filename##*.}|tr "A-Z" "a-z"` #拡張子（小文字）
       # アルバム名存在チェック
       google picasa list-albums |grep $albumname
       # アルバムがあればファイル名チェック
       if [ $? -eq 0 ];then
         google picasa list --title $albumname|cut -f1 -d,|grep $filename
-	# ファイルがアルバムになければPOST
-	if [ $? -eq 1 ];then
-	  sips -Z 2048 $file --out $TMPDIR/$filename
-	  google picasa post --title $albumname $TMPDIR/$filename 
-	  rm $TMPDIR/$filename
-	fi
-      # アルバムがなければ作成してUPLOAD
+        # ファイルがアルバムになければPOST
+        if [ $? -eq 1 ];then
+          if [ $ext -eq "jpg" ];then
+            sips -Z 2048 "$file" --out "$TMPDIR/$filename"
+          else
+            cp "$file" "$TMPDIR/$filename"
+          fi
+          google picasa post --title $albumname $TMPDIR/$filename 
+          rm $TMPDIR/$filename
+        fi
+        # アルバムがなければ作成してUPLOAD
       else
-	sips -Z 2048 $file --out $TMPDIR/$filename
-	google picasa create --title $albumname $TMPDIR/$filename
-	rm $TMPDIR/$filename
+        if [ $ext -eq "jpg" ];then
+          sips -Z 2048 "$file" --out "$TMPDIR/$filename"
+        else
+          cp "$file" "$TMPDIR/$filename"
+        fi
+        google picasa create --title $albumname "$TMPDIR/$filename"
+        rm "$TMPDIR/$filename"
       fi
     done
   fi
