82c893cf9a2ac618c4391143fd0f1a8d7bc64832
shooter@Mac <shooter@artemis.local>
2013-01-09 23:42:37 +0900

    関数化。

--- iPhoto2Picasa.sh
+++ iPhoto2Picasa.sh
@@ -10,7 +10,6 @@ PATH=$PATH:/usr/local/bin
 
 #コマンド存在チェック
 #google コマンドがなければ終了
-
 which google >/dev/null 2>&1
 if [ $? -ne 0 ];then
   echo "googlecl not found."
@@ -28,68 +27,70 @@ fi
 mkdir $TMPDIR 2>/dev/null ||(echo "Cannot run multiple." >&2;exit 9;)
 trap "rm -rf $TMPDIR;exit" 1 2 3 15
 
-#tmpファイルから日付を読む
-if [ -f $TMPFILE ];then
-  LASTDATE=`cat $TMPFILE`
-else
-  LASTDATE=`date +%Y%m%d000000`
-fi
-
-#iPhoto の Mastersフォルダから、該当日付以降のフォルダを検索
-cd "$IPHOTODIR"
-for dir in `find . -d 4 -print`
-do
-  targetdir=`basename "$dir"`
-  dirnum=`echo $targetdir|sed -e 's/-//'`
-  if [ "$dirnum" -gt $LASTDATE ];then
-    for file in $dir/* #フルパス
-    do
-      filename="`basename "$file"`" #ファイル名のみ
-      albumname=`stat -l -t %Y/%m/%d "$file"|cut -f6 -d" "`
-      ext=`echo ${filename##*.}|tr "A-Z" "a-z"` #拡張子（小文字）
-      # アルバム名存在チェック
-      google picasa list-albums |grep $albumname
-      # アルバムがあればファイル名チェック
-      if [ $? -eq 0 ];then
-        google picasa list --title $albumname|cut -f1 -d,|grep "$filename"
-        # ファイルがアルバムになければPOST
-        if [ $? -eq 1 ];then
-          if [ $ext = "jpg" ];then
-            sips -Z 2048 "$file" --out "$TMPDIR/$filename"
-            UPLOADFILE="$TMPDIR/$filename"
-          elif [ $ext = "mov" ];then
-            UPLOADFILE=$TMPDIR/"`echo $filename|sed -e 's/.mov/.mp4/'`"
-            ffmpeg -i "$file" -s 960x540 "$UPLOADFILE"
-          else
-            #cp "$file" "$TMPDIR/$filename"
-            UPLOADFILE="$file"
-          fi
-          google picasa post --title $albumname "$UPLOADFILE"
-          rm "$TMPDIR/$filename"
-        fi
-        # アルバムがなければ作成してUPLOAD
-      else
-        if [ $ext = "jpg" ];then
-          sips -Z 2048 "$file" --out "$TMPDIR/$filename"
-          UPLOADFILE="$TMPDIR/$filename"
-        elif [ $ext = "mov" ];then
-          UPLOADFILE=$TMPDIR/"`echo $filename|sed -e 's/.mov/.mp4/'`"
-          ffmpeg -i "$file" -s 960x540 "$UPLOADFILE"
-        else
-          #cp "$file" "$TMPDIR/$filename"
-          UPLOADFILE="$file"
-        fi
-        if [ -f "$UPLOADFILE" ];then
-          google picasa create --title $albumname "$UPLOADFILE"
-          if [ -f "$TMPDIR/$filename" ];then
-            rm "$TMPDIR/$filename"
-          fi
-        fi
-      fi
-    done
+#動画・静止画ファイルをリサイズする関数
+function fnc_resize()
+{
+  file=$1
+  filename="`basename "$file"`" #ファイル名のみ
+  ext=`echo ${filename##*.}|tr "A-Z" "a-z"` #拡張子（小文字）
+  if [ $ext = "jpg" ];then
+    sips -Z 2048 "$file" --out "$TMPDIR/$filename"
+    UPLOADFILE="$TMPDIR/$filename"
+  elif [ $ext = "mov" ];then
+    UPLOADFILE=$TMPDIR/"`echo $filename|sed -e 's/.mov/.mp4/'`"
+    ffmpeg -i "$file" -s 960x540 "$UPLOADFILE"
+  else
+    #cp "$file" "$TMPDIR/$filename"
+    UPLOADFILE="$file"
   fi
-done
-#最終アップロードDIRを出力。
-#echo `date +%Y%m%d%H%M%S` >$TMPFILE
-echo $dirnum >$TMPFILE
+}
+
+if [ $# -ge 1 ];then
+  for f in $*
+  do
+    fnc_resize $f
+  done
+else
+  #tmpファイルから日付を読む
+  if [ -f $TMPFILE ];then
+    LASTDATE=`cat $TMPFILE`
+  else
+    LASTDATE=`date +%Y%m%d000000`
+  fi
+
+  #iPhoto の Mastersフォルダから、該当日付以降のフォルダを検索
+  cd "$IPHOTODIR"
+  for dir in `find . -d 4 -print`
+  do
+    targetdir=`basename "$dir"`
+    dirnum=`echo $targetdir|sed -e 's/-//'`
+    if [ "$dirnum" -gt $LASTDATE ];then
+      for file in $dir/* #フルパス
+      do
+        filename="`basename "$file"`" #ファイル名のみ
+        albumname=`stat -l -t %Y/%m/%d "$file"|cut -f6 -d" "`
+        # アルバム名存在チェック
+        google picasa list-albums |grep $albumname
+        # アルバムがあればファイル名チェック
+        if [ $? -eq 0 ];then
+          google picasa list --title $albumname|cut -f1 -d,|grep "$filename"
+          # ファイルがアルバムになければPOST
+          if [ $? -eq 1 ];then
+            fnc_resize "$file"
+            google picasa post --title $albumname "$UPLOADFILE"
+          fi
+          # アルバムがなければ作成してUPLOAD
+        else
+          fnc_resize "$file"
+          if [ -f "$UPLOADFILE" ];then
+            google picasa create --title $albumname "$UPLOADFILE"
+          fi
+        fi
+      done
+    fi
+  done
+  #最終アップロードDIRを出力。
+  #echo `date +%Y%m%d%H%M%S` >$TMPFILE
+  echo $dirnum >$TMPFILE
+fi
 rm -rf $TMPDIR
